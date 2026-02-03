1cd8b571a15b38358ea68f6c5e7dfb8a42cd7ead
Odilxon <joniqulovodiljon30@gmail.com>
2026-02-03 19:12:36 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  server: {
+    host: true
+  },
   define: {
     // This ensures process.env is handled correctly in the browser build
     'process.env': {}
